d89a11d4ae8375d8413776a5436471d421f788f9
namrrac <namrrac+git@gmail.com>
2024-09-03 15:28:43 -0700

    Edited main method.

--- Business.java
+++ Business.java
@@ -1,5 +1,33 @@
 public class Business {
+    private String first;
+    private String last;
+
+    public Business(String first, String last){
+        this.first = first;
+        this.last = last;
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getLast() {
+        return last;
+    }
+
+    public void setLast(String last) {
+        this.last = last;
+    }
     public static void main(String[] args) {
-        System.out.println("atests 3");
+        System.out.println("A tests 3!");
+        Business bus1 = new Business("THISname", "Thatname");
+        Business bus2 = new Business(bus1.getFirst(), bus1.getLast());
+        Business bus3 = bus1;
+        System.out.println(bus3.getFirst());
+        System.out.println(bus3.getLast());
     }
 }
---
+++ Hacker.java
@@ -0,0 +1,8 @@
+public class Hacker {
+    public static void main(String[] args) {
+        Business bus1 = new Business("Jason", "Carr");
+        System.out.println(bus1.getFirst());
+        System.out.println(bus1.getLast());
+        bus1.;
+    }
+}
