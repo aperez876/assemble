b45ab75d6fb257698b7c537019d0eea26d1adb0a
Ricardo M <95rikym@gmail.com>
2024-09-12 16:02:09 -0700

    began skeleton from Mermaid.live

--- Business.java
+++ Business.java
@@ -1,4 +1,13 @@
 public class Business {
+    String firstName;
+    String lastName;
+
+    public Business (String fName, String lName) {
+        this.firstName = fName;
+        this.lastName = lName;
+
+    }
+
     public static void main(String[] args) {
         System.out.println("atests 3");
     }
---
+++ src/main/java/Follow.java
@@ -0,0 +1,13 @@
+public class Follow {
+    private int followId;
+    private int customerId;
+    private int businessId;
+
+    public void followBusiness() {
+        // Implementation here
+    }
+
+    public void unfollowBusiness() {
+        // Implementation here
+    }
+}
