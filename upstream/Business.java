e3c9d57a1ba7c0d0750211d328c124042ea74838
namrrac <namrrac+git@gmail.com>
2024-09-12 15:28:38 -0700

    Added files to "upstream" folder.

---
+++ upstream/Business.java
@@ -0,0 +1,5 @@
+public class Business {
+    public static void main(String[] args) {
+        System.out.println("atests 3");
+    }
+}
---
+++ upstream/Hacker.java
@@ -0,0 +1,7 @@
+public class Hacker {
+    public static void main(String[] args) {
+        Business bus1 = new Business("Jason", "Carr");
+        System.out.println(bus1.getFirst());
+        System.out.println(bus1.getLast());
+    }
+}
