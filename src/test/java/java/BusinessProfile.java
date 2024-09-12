1bf65a278a3580c08cf1752ba1dcffb993e846e7
namrrac <namrrac+git@gmail.com>
2024-09-12 16:23:55 -0700

    Changes test folder.

---
+++ src/test/java/java/BusinessProfile.java
@@ -0,0 +1,19 @@
+package java;
+
+import java.util.List;
+
+public class BusinessProfile {
+    private int profileId;
+    private String name;
+    private String openingTimes;
+    private String location;
+    private List<String> gamesAvailable;
+
+    public void updateProfile() {
+        // Implementation here
+    }
+
+    public void getProfileDetails() {
+        // Implementation here
+    }
+}
